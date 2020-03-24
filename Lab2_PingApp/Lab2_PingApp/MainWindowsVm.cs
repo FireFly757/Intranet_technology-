2a1860039b8b4c8030bed0b2749f26f8c7bb50fd
firefly <vadik777k@gmail.com>
2020-03-24 20:22:12 +0500

    Сделал начальный вариант представлений и VM. Теперь можно запускать главное окно и что-то потыкать

---
+++ Lab2_PingApp/Lab2_PingApp/MainWindowsVm.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using Lab2_PingApp.PingControl;
+
+namespace Lab2_PingApp
+{
+    class MainWindowVm : INotifyPropertyChanged
+    {
+        public MainWindowVm()
+        {
+            PingControlVm = new PingControlVM();
+        }
+
+        private PingControlVM _pingControlVm;
+        public PingControlVM PingControlVm
+        {
+            get { return _pingControlVm; }
+            set
+            {
+                _pingControlVm = value;
+                OnPropertyChanged("PingControlVm");
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName]string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+
+    }
+}
---
+++ Lab2_PingApp/Lab2_PingApp/RequestItem.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2_PingApp
+{
+    class RequestItem
+    {
+        public string IpAddress { get; set; }
+        public int RoadtripTime { get; set; }
+
+        public RequestItem(string ipAddress, int roadtripTime)
+        {
+            IpAddress = ipAddress;
+            RoadtripTime = roadtripTime;
+        }
+    }
+}
